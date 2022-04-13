743b62863243e0b6c19363ca45d000a43fdd8e0f
TCSE <talik@tcse-cms.com>
2022-04-13 21:52:01 +0300

    добавлены новые стили галереи
    все те, что есть на демо-страницах
    
    https://unitegallery.net/index.php?page=tiles-columns-options
    
    https://unitegallery.net/index.php?page=tiles-justified-options
    
    https://unitegallery.net/index.php?page=tiles-nested-options
    
    https://unitegallery.net/index.php?page=tilesgrid-options
    
    https://unitegallery.net/index.php?page=carousel-options
    
    https://unitegallery.net/index.php?page=default-options
    
    https://unitegallery.net/index.php?page=compact-options
    
    https://unitegallery.net/index.php?page=grid-options
    
    https://unitegallery.net/index.php?page=slider-options
    
    https://unitegallery.net/index.php?page=video-items-syntax

--- templates/{THEME}/assets/unitegallery/themes/compact/ug-theme-compact.js
+++ templates/{THEME}/assets/unitegallery/themes/compact/ug-theme-compact.js
@@ -1,90 +1,102 @@
 
 if(typeof g_ugFunctions != "undefined")
-	g_ugFunctions.registerTheme("grid");
+	g_ugFunctions.registerTheme("compact");
 else 
-	jQuery(document).ready(function(){g_ugFunctions.registerTheme("grid")});
+	jQuery(document).ready(function(){g_ugFunctions.registerTheme("compact")});
 
 
 /**
  * Grid gallery theme
  */
-function UGTheme_grid(){
+function UGTheme_compact(){
 
 	var t = this;
 	var g_gallery = new UniteGalleryMain(), g_objGallery, g_objects, g_objWrapper; 
 	var g_objSlider;
 	var g_functions = new UGFunctions();
-	var g_objPanel = new UGGridPanel();
-
+	var g_objPanel = new UGStripPanel();
 	
 	//theme options
 	var g_options = {
 			theme_load_slider:true,					//true, false - load the slider
 			theme_load_panel:true,					//true, false - load the thumbs grid panel
-			theme_panel_position: "right",			//left, right, top, bottom - thumbs panel position
+			theme_panel_position: "bottom",			//top, bottom, left, right - thumbs panel position
 			theme_hide_panel_under_width: 480		//hide panel under certain browser width, if null, don't hide
 	};
 	
-	//global defaults (for right side)
+	//global defaults
 	var g_defaults = {
 			slider_controls_always_on:true,
-			slider_vertical_scroll_ondrag: true
+			slider_enable_text_panel:false,
+			slider_vertical_scroll_ondrag: true,
+			strippanel_enable_buttons:false			
 	};
+	
 	
 	//special defaults for left side panel position
 	var g_defaults_left = {
+		slider_enable_text_panel:true,
 		slider_zoompanel_align_hor: "right",
 		slider_fullscreen_button_align_hor: "right",
 		slider_play_button_align_hor: "right",
-		
 		slider_zoompanel_offset_vert: 9,
 		slider_zoompanel_offset_hor: 11,
 		slider_play_button_offset_hor: 88,
 		slider_play_button_offset_vert: 8,
 		slider_fullscreen_button_offset_hor: 52,
-		slider_fullscreen_button_offset_vert: 9								
-	};
-	
-	var g_defaults_right = {
-			slider_zoompanel_offset_vert: 9,
-			slider_zoompanel_offset_hor: 11,
-			slider_play_button_offset_hor: 88,
-			slider_play_button_offset_vert: 8,
-			slider_fullscreen_button_offset_hor: 52,
-			slider_fullscreen_button_offset_vert: 9						
+		slider_fullscreen_button_offset_vert: 9,
+		slider_progress_indicator_align_hor: "right",
+		slider_progress_indicator_offset_vert: 36,	
+		slider_progress_indicator_offset_hor: 63			
 	}
 	
+	//special defaults for right side panel position
+	var g_defaults_right = {
+		slider_enable_text_panel:true,
+		slider_zoompanel_offset_vert: 9,
+		slider_zoompanel_offset_hor: 11,
+		slider_play_button_offset_hor: 88,
+		slider_play_button_offset_vert: 8,
+		slider_fullscreen_button_offset_hor: 52,
+		slider_fullscreen_button_offset_vert: 9,
+		
+		slider_progress_indicator_align_hor: "left",
+		slider_progress_indicator_offset_vert: 36,	
+		slider_progress_indicator_offset_hor: 63	
+		
+	}
+	
+	//special defaults for bottom panel position
 	var g_defaults_bottom = {
-		slider_enable_text_panel: false,
+		
 		slider_zoompanel_align_hor: "right",
 		slider_zoompanel_offset_vert: 10,
 		
 		slider_progress_indicator_align_hor: "left",
 		slider_progress_indicator_offset_vert: 36,	
-		slider_progress_indicator_offset_hor: 16
+		slider_progress_indicator_offset_hor: 16	
 	}
 	
+	//special defaults for top panel position
 	var g_defaults_top = {
-			slider_enable_text_panel: false,
-			slider_zoompanel_align_vert: "bottom",
-			slider_zoompanel_offset_vert: 10,
-			
-			slider_play_button_align_hor: "right",
-			slider_play_button_align_vert: "bottom",
-			
-			slider_fullscreen_button_align_vert: "bottom",	
-			slider_fullscreen_button_align_hor: "right",
-			
-			slider_progress_indicator_align_vert: "bottom",
-			slider_progress_indicator_offset_vert: 40,			
-			gridpanel_padding_border_top: 4,		    //padding between the top border of the panel
-			gridpanel_padding_border_bottom: 10			//padding between the bottom border of the panel	
+		slider_zoompanel_align_vert: "bottom",
+		slider_zoompanel_offset_vert: 10,
+		
+		slider_play_button_align_hor: "right",
+		slider_play_button_align_vert: "bottom",
+		
+		slider_fullscreen_button_align_vert: "bottom",	
+		slider_fullscreen_button_align_hor: "right",
+		
+		slider_progress_indicator_align_vert: "bottom",
+		slider_progress_indicator_offset_vert: 40
 	}
+	
 	
 	//temp variables
 	var g_temp = {
-		isMobileModeWasEnabled: false,
-		isHorPos: false
+		isVertical: false,
+		isMobileModeWasEnabled: false
 	};
 	
 	
@@ -95,38 +107,18 @@ function UGTheme_grid(){
 		
 		g_gallery = gallery;
 		
-		g_options = jQuery.extend(g_options, g_defaults);
 		g_options = jQuery.extend(g_options, customOptions);
-		
-		switch(g_options.theme_panel_position){
-			case "left":
-				g_options = jQuery.extend(g_options, g_defaults_left);
-			break;
-			case "right":
-				g_options = jQuery.extend(g_options, g_defaults_right);
-			break;
-			case "top":
-				g_options = jQuery.extend(g_options, g_defaults_top);
-			break;
-			case "bottom":
-				g_options = jQuery.extend(g_options, g_defaults_bottom);
-			break;			
-		}
-		
-		g_options = jQuery.extend(g_options, customOptions);
-		
-		modifyOptions();
+				
+		modifyOptions(customOptions);
 		
 		//set gallery options
 		g_gallery.setOptions(g_options);
 				
 		//include gallery elements
 		if(g_options.theme_load_panel == true){
-			if(g_options.theme_panel_position == "top" || g_options.theme_panel_position == "bottom")
-				g_temp.isHorPos = true;
-				
-			g_objPanel.setOrientation(g_options.theme_panel_position);
 			g_objPanel.init(gallery, g_options);
+			g_objPanel.setOrientation(g_options.theme_panel_position);
+			
 		}else
 			g_objPanel = null;
 		
@@ -148,12 +140,41 @@ function UGTheme_grid(){
 	/**
 	 * modify options
 	 */
-	function modifyOptions(){
+	function modifyOptions(customOptions){
+				
+		g_options = jQuery.extend(g_options, g_defaults);
 		
-		if(g_options.theme_load_panel == true && g_options.theme_panel_position == "left"){
-			g_options.gridpanel_handle_position = "right";
+		if(g_options.theme_load_panel == true){
+			
+			switch(g_options.theme_panel_position){
+				case "left":
+				case "right":
+					g_temp.isVertical = true;
+					g_options.strippanel_vertical_type = true;
+				break;
+			}
 		}
+		
+		
+		switch(g_options.theme_panel_position){
+			case "left":
+				g_options = jQuery.extend(g_options, g_defaults_left);				
+			break;
+			case "right":
+				g_options = jQuery.extend(g_options, g_defaults_right);				
+			break;
+			case "top":
+				g_options = jQuery.extend(g_options, g_defaults_top);
+			break;
+			case "bottom":
+				g_options = jQuery.extend(g_options, g_defaults_bottom);
+			break;
+		}
+		
+		g_options = jQuery.extend(g_options, customOptions);
+		
 	}
+	
 	
 	/**
 	 * init all the theme's elements and set them to their places 
@@ -212,16 +233,18 @@ function UGTheme_grid(){
 	 * init size of the thumbs panel
 	 */
 	function initThumbsPanel(){
-				
+		
 		//set size:
 		var objGallerySize = g_gallery.getSize();
-		if(g_temp.isHorPos == true)
+			
+		if(g_temp.isVertical == false)			
 			g_objPanel.setWidth(objGallerySize.width);
 		else
 			g_objPanel.setHeight(objGallerySize.height);
-					
+		
 		g_objPanel.run();
 	}
+	
 	
 	
 	/**
@@ -229,31 +252,45 @@ function UGTheme_grid(){
 	 */
 	function placeThumbsPanel(){
 		
-		var objPanelElement = g_objPanel.getElement();
-		
 		var isNeedToHide = isPanelNeedToHide();
 		var isHidden = g_objPanel.isPanelClosed();		
 		
-		var showClosed = (isNeedToHide || isHidden);
-
-		var pos = g_options.theme_panel_position;
+		var objPanelElement = g_objPanel.getElement();
+		var posVert = "bottom";
+		var posHor = "left";
 		
-		//place the panel closed or opened state.
+		var showClosed = (isNeedToHide || isHidden);
+			
 		if(showClosed){
 			var hiddenDest = g_objPanel.getClosedPanelDest();
 			var originalPos =  g_functions.getElementRelativePos(objPanelElement, g_options.theme_panel_position);
 			g_objPanel.setClosedState(originalPos);
-			pos = hiddenDest;
 		}else{
 			g_objPanel.setOpenedState();
 		}
 		
-		if(g_temp.isHorPos == true)		
-			g_functions.placeElement(objPanelElement, "left", pos);
-		else
-			g_functions.placeElement(objPanelElement, pos, "top");
+		switch(g_options.theme_panel_position){
+			case "right":
+			case "left":
+				posHor = g_options.theme_panel_position;
+				if(showClosed)
+					posHor = hiddenDest;
+				
+			break;		
+			case "top":
+			case "bottom":				
+				posVert = g_options.theme_panel_position;
+				if(showClosed)
+					posVert = hiddenDest;
+			break;
+			default:
+				throw new Error("Wrong panel position: " + g_options.theme_panel_position);
+			break;
+		}
 		
+		g_functions.placeElement(objPanelElement, posHor, posVert, 0, 0);
 	} 
+	
 	
 	
 	/**
@@ -272,11 +309,11 @@ function UGTheme_grid(){
 		if(g_objPanel){
 			
 			var panelSize = g_objPanel.getSize();
-						
+			
 			switch(g_options.theme_panel_position){
 				case "left":
 					sliderLeft = panelSize.right;
-					sliderWidth = gallerySize.width - panelSize.right;				
+					sliderWidth = gallerySize.width - panelSize.right;	
 				break;
 				case "right":
 					sliderWidth = panelSize.left;					
@@ -286,17 +323,16 @@ function UGTheme_grid(){
 					sliderTop = panelSize.bottom;
 				break;
 				case "bottom":
-					sliderHeight = panelSize.top;					
+					sliderHeight = panelSize.top;
 				break;
 			}
-						
+			
 		}
-				
+		
 		g_objSlider.setSize(sliderWidth, sliderHeight);
 		g_objSlider.setPosition(sliderLeft, sliderTop);
 	}
 
-	
 	/**
 	 * check if need to hide the panel according the options.
 	 */
@@ -313,6 +349,7 @@ function UGTheme_grid(){
 			
 		return(false);
 	}
+
 	
 	/**
 	 * check if need to hide or show panel according the theme_hide_panel_under_width option
@@ -344,7 +381,7 @@ function UGTheme_grid(){
 	function onSizeChange(){
 		
 		initAndPlaceElements();
-		
+				
 		if(g_objPanel)
 			checkHidePanel();
 		
@@ -355,10 +392,11 @@ function UGTheme_grid(){
 	 * on panel move event
 	 */
 	function onPanelMove(){
+		
 		placeSlider();
 	}
 
-
+	
 	/**
 	 * before items request: hide items, show preloader
 	 */
@@ -367,16 +405,16 @@ function UGTheme_grid(){
 		g_gallery.showDisabledOverlay();
 	
 	}
-	
+		
 	
 	/**
 	 * init buttons functionality and events
 	 */
 	function initEvents(){
 						
-		g_objGallery.on(g_gallery.events.SIZE_CHANGE,onSizeChange);		
+		g_objGallery.on(g_gallery.events.SIZE_CHANGE,onSizeChange);
 		g_objGallery.on(g_gallery.events.GALLERY_BEFORE_REQUEST_ITEMS, onBeforeReqestItems);
-	
+		
 		if(g_objPanel){
 			jQuery(g_objPanel).on(g_objPanel.events.FINISH_MOVE, onPanelMove);
 		}
@@ -385,11 +423,11 @@ function UGTheme_grid(){
 	
 	
 	/**
-	 * destroy the theme events
+	 * destroy the theme
 	 */
 	this.destroy = function(){
 		
-		g_objGallery.off(g_gallery.events.SIZE_CHANGE,onSizeChange);		
+		g_objGallery.off(g_gallery.events.SIZE_CHANGE);
 		g_objGallery.off(g_gallery.events.GALLERY_BEFORE_REQUEST_ITEMS);
 		
 		if(g_objPanel)
@@ -397,7 +435,7 @@ function UGTheme_grid(){
 		
 		g_objPanel.destroy();
 		g_objSlider.destroy();
-	
+		
 	}
 	
 	
